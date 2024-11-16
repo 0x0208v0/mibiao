14192aec66ca100257b804ac53587bcd4e74a2b0
0x0208 Version 0 <70836233+0x0208v0@users.noreply.github.com>
2024-11-16 22:32:01 +0800

    update

--- src/mibiao/config.py
+++ src/mibiao/config.py
@@ -1,20 +1,50 @@
 import os
+import warnings
+from dataclasses import asdict
+from dataclasses import dataclass
 
-from pydantic_settings import BaseSettings
-from pydantic_settings import SettingsConfigDict
-
-
-class Config(BaseSettings):
-    model_config = SettingsConfigDict(extra='ignore', env_file=os.getenv('ENV_FILE', '.env'))
-
-    LOGGING_LEVEL: str = 'INFO'
-    LOGGING_FORMAT: str = '[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s'
-
-    SECRET_KEY: str = 'mibiao'
-    PERMANENT_SESSION_LIFETIME_MINUTES: int = 10
-
-    SQLALCHEMY_DATABASE_URI: str = 'sqlite:///db.sqlite3'
-    SQLALCHEMY_ECHO: bool = False
+from dotenv import load_dotenv
 
 
-config = Config()
+@dataclass
+class Config:
+    LOGGING_LEVEL: str
+    LOGGING_FORMAT: str
+
+    SECRET_KEY: str
+    PERMANENT_SESSION_LIFETIME_MINUTES: int
+
+    SQLALCHEMY_DATABASE_URI: str
+    SQLALCHEMY_ECHO: bool
+
+    def to_dict(self) -> dict:
+        return {k: str(v) for k, v in asdict(self).items()}
+
+
+def load_config(env: str = '.env') -> Config | None:
+    if not load_dotenv(env):
+        warnings.warn(
+            f'{env} not found, using default config, current working directory: {os.getcwd()}'
+        )
+
+    return Config(
+        LOGGING_LEVEL=os.environ.get('LOGGING_LEVEL', 'INFO'),
+        LOGGING_FORMAT=os.environ.get(
+            'LOGGING_FORMAT',
+            '[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
+        ),
+        SECRET_KEY=os.environ.get('SECRET_KEY', 'mibiao'),
+        PERMANENT_SESSION_LIFETIME_MINUTES=int(
+            os.environ.get('PERMANENT_SESSION_LIFETIME_MINUTES', 10)
+        ),
+        SQLALCHEMY_DATABASE_URI=os.environ.get(
+            'SQLALCHEMY_DATABASE_URI', 'sqlite:///db.sqlite3'
+        ),
+        SQLALCHEMY_ECHO=os.environ.get('SQLALCHEMY_ECHO', False),
+    )
+
+
+config = load_config()
+
+if __name__ == '__main__':
+    print(config.SQLALCHEMY_DATABASE_URI)
--- src/mibiao/models/setting.py
+++ src/mibiao/models/setting.py
@@ -1,18 +1,17 @@
-import uuid
-
+from sqlalchemy import Integer
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
 from mibiao.models.base import BaseModel
-from mibiao.models.base import UUID
 
 
 class Setting(BaseModel):
-    user_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(),
+    user_id: Mapped[int] = mapped_column(
+        Integer,
         nullable=False,
     )
+
     user: Mapped[str] = mapped_column(
         String(128),
         nullable=False,

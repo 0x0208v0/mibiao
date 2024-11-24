fd9aadf5661fe8b5a2e6ffea0508fe6dd012066c
0x0208 Version 0 <70836233+0x0208v0@users.noreply.github.com>
2024-11-24 14:59:53 +0800

    update

--- src/mibiao/blueprints/main.py
+++ src/mibiao/blueprints/main.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask import render_template
 
 from mibiao.models.domain import Domain
-from mibiao.models.tag import Tag
+from mibiao.models.tag import Tag, DomainTag
 
 blueprint = Blueprint('main', __name__, url_prefix='/')
 
@@ -21,15 +21,28 @@ def index():
             Domain.id.desc(),
         ],
     )
-    tag_list = Tag.get_list(
-        Tag.is_hide == False,
-        order_by=[
-            Tag.rank,
-            Tag.id.desc(),
-        ],
-    )
     return render_template(
         'main/index.html',
         domain_list=domain_list,
-        tag_list=tag_list,
+    )
+
+
+@blueprint.get('/<string:url_path_name>')
+def index_by_tag(url_path_name: str):
+    tag = Tag.get_one(Tag.url_path_name == url_path_name)
+    if tag:
+        domain_list = Domain.get_list(
+            DomainTag.domain_id == Domain.id,
+            DomainTag.tag_id == tag.id,
+            Domain.is_hide == False,
+            order_by=[
+                Domain.rank,
+                Domain.id.desc(),
+            ],
+        )
+    else:
+        domain_list = []
+    return render_template(
+        'main/index.html',
+        domain_list=domain_list,
     )

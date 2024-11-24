4a7da7eddaa6a7969f93857c6a70f608ba6c881b
0x0208 Version 0 <70836233+0x0208v0@users.noreply.github.com>
2024-11-24 23:57:23 +0800

    update

--- src/mibiao/utils/markdown.py
+++ src/mibiao/utils/markdown.py
@@ -7,7 +7,7 @@ from mistune import safe_entity
 
 class MyRenderer(HTMLRenderer):
     def link(self, text: str, url: str, title=None) -> str:
-        s = '<a class="text-[#0dbc79]" href="' + self.safe_url(url) + '"'
+        s = '<a class="text-[#55b786] hover:text-[#45ca6b]" href="' + self.safe_url(url) + '"'
         if title:
             s += ' title="' + safe_entity(title) + '"'
         return s + '>' + text + '</a>'
